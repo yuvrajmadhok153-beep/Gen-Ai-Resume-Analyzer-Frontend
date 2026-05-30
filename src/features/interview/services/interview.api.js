0bdc4eae10de25ad26ae176c10658075e04f9c16
YURAJ MADHOK <yuvrajmadhok153@gmail.com>
2026-05-30 15:41:56 +0530

    update the local urls

--- src/features/interview/services/interview.api.js
+++ src/features/interview/services/interview.api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: "https://gen-ai-resume-analyzer-backend-one.vercel.app",
     withCredentials: true,
 })
 
